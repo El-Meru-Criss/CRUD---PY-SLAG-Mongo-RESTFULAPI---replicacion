65d2789daa63cd3b1717cae4822764e5201695cd
brayan1851258 <89151022+brayan1851258@users.noreply.github.com>
2024-04-10 18:48:57 -0500

    Add files via upload

--- flask/app.py
+++ flask/app.py
@@ -1,18 +1,27 @@
+# Importamos las clases Flask y render_template desde el paquete Flask
 from flask import Flask, render_template
 
-app=Flask(__name__)
+# Creamos una instancia de la clase Flask y la asignamos a la variable 'app'
+app = Flask(__name__)
 
+# Definimos una ruta principal '/' utilizando el decorador @app.route('/')
 @app.route('/')
 def index():
-    nombres=['brayan', 'steven', 'cristian', 'ana', 'sebastian', 'jaider', 'harold', 'david', 'felipe', 'maicol']
+    # Lista de nombres
+    nombres = ['brayan', 'steven', 'cristian', 'ana', 'sebastian', 'jaider', 'harold', 'david', 'felipe', 'maicol']
 
-    data={
-        'titulo':'index',
-        'tabla':'tabla',
-        'nombres':nombres,
-        'numero_nombres':len(nombres)
+    # Creamos un diccionario 'data' con varios datos
+    data = {
+        'titulo': 'index',                # Título de la página
+        'tabla': 'tabla',                 # Nombre de la tabla (aunque aquí no está siendo utilizado)
+        'nombres': nombres,               # Lista de nombres
+        'numero_nombres': len(nombres)    # Número de nombres en la lista
     }
+
+    # Renderizamos la plantilla HTML 'index.html' y pasamos el diccionario 'data' como contexto
     return render_template('index.html', data=data)
 
+# Bloque que ejecuta la aplicación solo si el script se ejecuta directamente (no si se importa como un módulo)
 if __name__ == '__main__':
+    # Iniciamos el servidor Flask en modo de depuración (debug=True)
     app.run(debug=True)
